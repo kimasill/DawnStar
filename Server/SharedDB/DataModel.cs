eed2b7750ad0be8a9949bc666d781b33ac46ec7d
kimasill <80378085+kimasill@users.noreply.github.com>
2024-08-07 21:37:30 +0900

    Server Page

---
+++ Server/SharedDB/DataModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedDB
+{
+    [Table("Token")]
+    public class TokenDb
+    {
+        public int TokenDbId { get; set; }
+        public int AccountDbId { get; set; }
+        public int Token { get; set; }
+        public DateTime Expired { get; set; }
+    }
+
+    [Table("ServerInfo")]
+    public class ServerDb
+    {
+        public int ServerDbId { get; set; }
+        public string Name { get; set; }
+        public string IpAdress { get; set; }
+        public int Port { get; set; }
+        public int BusyScore { get; set; }
+    }
+}
---
+++ Server/SharedDB/SharedDbContext.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedDB
+{
+    public class SharedDbContext : DbContext
+    {
+        public DbSet<TokenDb> Tokens { get; set; }
+        public DbSet<ServerDb> Servers { get; set; }
+        //GameServer
+        public SharedDbContext()
+        {
+
+        }
+        //ASP .NET
+        public SharedDbContext(DbContextOptions<SharedDbContext> options) : base(options)
+        {
+
+        }
+        public static string ConnectionString { get; set;} = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=SharedDB;Integrated Security=True";
+        //GameServer
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            if(options.IsConfigured == false)
+            {
+                options
+                    .UseSqlServer(ConnectionString);
+            }
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<TokenDb>()
+                .HasIndex(t => t.AccountDbId)
+                .IsUnique();
+
+            builder.Entity<ServerDb>()
+                .HasIndex(s => s.Name)
+                .IsUnique();
+        }
+    } 
+
+    
+}
